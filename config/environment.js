3827e3f473ded8b9f1239797843732dc534cef9a
dennismuturia <dmuturia2922@gmail.com>
2017-07-11 16:40:53 +0300

    Initial Commit

--- config/environment.js
+++ config/environment.js
@@ -12,12 +12,21 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
+
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyA6KWlyHUAUJ0qfQRqRYk1zQpH4hn6PgPI",
+        authDomain: "rentals-86080.firebaseapp.com",
+        databaseURL: "https://rentals-86080.firebaseio.com",
+        projectId: "rentals-86080",
+        storageBucket: "rentals-86080.appspot.com"
+
+  },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
